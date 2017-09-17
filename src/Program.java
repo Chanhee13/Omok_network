b7a5aedf10245ca8adbba17647864037221577f7
chanheepark <ch13p@naver.com>
2017-09-17 17:08:20 +0900

    에코

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,45 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Program {
     public static void main(String[] args){
-        System.out.println("hello World");
+                try {
+                    ServerSocket severSocket = new ServerSocket(6000);
 
-        System.out.println("멍청아");
-    }
+                    while (true) {
+                        Socket socket = severSocket.accept();
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    String remoteAddress = socket.getRemoteSocketAddress().toString();
+                                    System.out.println(remoteAddress);
+
+                                    InputStream is = socket.getInputStream();
+                                    OutputStream os = socket.getOutputStream();
+
+                                    while (true) {
+                                        byte[] data = new byte[1024];
+                                        int len = is.read(data);
+                                        if (len == -1) {
+                                            System.out.println("연결이 종료되었습니다.");
+                                            break;
+                                        }
+                                        os.write(data, 0, len);
+                                    }
+                                    socket.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 }
